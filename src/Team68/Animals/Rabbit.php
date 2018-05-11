b60f3c6ddda9cb04d90de53c26b436795c6999e6
unknown <teacher@r68-teacher.lab05.ifmi.ddpu>
2018-05-11 09:59:04 +0300

    init

---
+++ src/Team68/Animals/Rabbit.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Team68\Animals;
+
+class Rabbit (){
+	
+	function run (){
+		
+		echo "Rabbit is running";
+		
+	}
+	
+	
+	function stop (){
+		
+		echo "Rabbit has stopped";
+		
+	}
+	
+}
